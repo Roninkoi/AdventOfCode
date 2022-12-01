32ecb4c7c493348b96ddebf452cefc912e90d171
Roni Koitermaa <roninkoi@iki.fi>
2022-12-01 21:12:55 +0200

    Day 1 part 2

--- src/day1.rs
+++ src/day1.rs
@@ -30,3 +30,34 @@ pub fn day1_1() {
 
     println!("Elf {elf_max} has the most calories: {cal_max}");
 }
+
+pub fn day1_2() {
+    let stdin = std::io::stdin();
+    let mut lines = stdin.lock().lines();
+
+    let mut elf_calories = Vec::<i32>::new();
+    let mut total_calories = 0;
+    while let Some(line) = lines.next() {
+        let l = line.unwrap();
+        if l.len() > 0 {
+            let v = l.parse::<i32>().unwrap();
+            total_calories += v;
+        } else {
+            elf_calories.push(total_calories);
+            total_calories = 0;
+        }
+    }
+    elf_calories.push(total_calories);
+
+    elf_calories.sort();
+    elf_calories.reverse();
+
+    let mut cal_sum = 0;
+    for i in 0..3 {
+        let cal = elf_calories[i];
+        println!("{cal}");
+        cal_sum += cal;
+    }
+
+    println!("Top 3 total calories: {cal_sum}");
+}
