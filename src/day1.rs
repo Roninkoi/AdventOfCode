f3caa601c324114b80bbdf81b7d951e737ec4928
Roni Koitermaa <roninkoi@iki.fi>
2022-12-01 20:59:58 +0200

    Day 1 part 1

---
+++ src/day1.rs
@@ -0,0 +1,32 @@
+use std::io::BufRead;
+
+pub fn day1_1() {
+    let stdin = std::io::stdin();
+    let mut lines = stdin.lock().lines();
+
+    let mut elf_calories = Vec::<i32>::new();
+    let mut total_calories = 0;
+    while let Some(line) = lines.next() {
+        let l = line.unwrap();
+        if l.len() > 0 {
+            let v = l.parse::<i32>().unwrap();
+            total_calories += v;
+        } else {
+            elf_calories.push(total_calories);
+            total_calories = 0;
+        }
+    }
+    elf_calories.push(total_calories);
+
+    let mut elf_max = 0;
+    let mut cal_max = 0;
+    for (elf, cal) in elf_calories.iter().enumerate() {
+        println!("{elf}, {cal}");
+        if *cal > cal_max {
+            cal_max = *cal;
+            elf_max = elf;
+        }
+    }
+
+    println!("Elf {elf_max} has the most calories: {cal_max}");
+}
